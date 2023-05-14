1466b132d66c587b6befa8dbec60f2ef802e3443
Arielle Montgomery <Armontgomery07@gmail.com>
2023-05-14 12:35:05 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value;
+
+    
+    const taskItem = document.createElement('li');
+    taskItem.innerText = taskDescription;
+    taskList.appendChild(taskItem);
+
+    
+    taskInput.value = '';
+  });
 });
+
+
